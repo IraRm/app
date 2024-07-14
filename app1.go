cd179bfc6329427f7013e80a872e3bd671fbe01c
IraRm <irina.sg@icloud.com>
2024-07-14 20:56:31 +0300

    second

--- app1.go
+++ app1.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	nums := []int{1, 2, 3, 4, 5}
+	nums := []int{1, 2, 2, 4, 5}
 	numsRev := []int{}
 	for i := len(nums) - 1; i >= 0; i-- {
 		numsRev = append(numsRev, (nums[i]))
